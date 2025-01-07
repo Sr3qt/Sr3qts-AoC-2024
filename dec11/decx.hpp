4ef779503d910a8fecc3748e7e7963a39565a8cb
Sr3qt <105977398+Sr3qt@users.noreply.github.com>
2025-01-08 00:16:28 +0100

    dec11 part 2 temp

--- dec11/decx.hpp
+++ dec11/decx.hpp
@@ -6,7 +6,7 @@
 #include <chrono>
 
 // NEW INCLUDES
-
+#include <unordered_map>
 
 #define CURRENT_DAY 11
 
@@ -69,25 +69,96 @@ static long long solve1() {
 	for (int i = 0; i < 25; i++) {
 		iterate(temp, temp2, running_count);
 		temp.swap(temp2);
+		// std::cout << running_count << std::endl;
 	}
 
 	res = running_count;
 	return res;
 }
 
+
+// First attmempt: 54:11
+// Recursive memoization was faster, but not the right approach.
+// Searched online and it seems that grouping works better.
+// After more researched i found a guy who also solved it using memoization.
+// My implementation must be bugged, will try to fix.
+long long recursive(
+	long long num,
+	int depth,
+	std::unordered_map<long long, std::unordered_map<int, long long>>& memory) {
+
+	// std::cout << "Called: " << num << ", " << depth << "\n";
+
+	if (memory.find(num) != memory.end()) {
+		// Value has already been calculated.
+		if (memory[num].find(depth) != memory[num].end()) {
+			// Value has already been calculated at this depth.
+			// std::cout << "Remembered value!\n";
+			return memory[num][depth];
+		}
+		// auto temp = recursive(num, depth + 1, memory);
+		// memory[num] = {{depth, temp}};
+		// return temp;
+	}
+
+	// Base case. Set number of steps
+	if (depth == 30) {
+		// std::cout << "base cas found\n";
+		return 1 + int(!(floor(log10(num) + 1 % 2)));
+	} else {
+		// Recursively try to find solutions
+		if (num == 0) {
+			auto temp = recursive(1, depth + 1, memory);
+			memory[num] = {{depth, temp}};
+			return temp;
+		}
+		if ((long long)floor(log10(num) + 1) % 2 == 0) {
+			auto item = std::to_string(num);
+			auto right = std::to_string(stoull(item.substr(item.size() / 2)));
+			auto left = item.substr(0, item.size() / 2);
+
+			// If left and right are same, memoization will kick in. Calling twice is fine.
+			auto temp1 = recursive(stoll(left), depth + 1, memory);
+			auto temp2 = recursive(stoll(right), depth + 1, memory);
+			memory[num] = {{depth, temp1 + temp2}};
+			return temp1 + temp2;
+		} else {
+			auto temp = recursive(num * 2024, depth + 1, memory);
+			memory[num] = {{depth, temp}};
+			return temp;
+		}
+	}
+}
+
+
+// Need recursive memoization bullshit to complete
 static long long solve2() {
 	std::string myText;
 	std::string file = std::string(__FILE__);
 	std::fstream MyReadFile(file.substr(0, file.rfind(OS_SEP)) + OS_SEP + "data.txt");
 	long long res = 0;
 
-
-	while (getline (MyReadFile, myText)) {
-
-	}
+	getline (MyReadFile, myText);
 
 	MyReadFile.close();
 
+	std::unordered_map<long long, std::unordered_map<int, long long>> memory;
+	auto temp = split(myText);
+	for (auto const& num : temp) {
+		res += recursive(stoll(num), 0, memory);
+	}
+	// std::vector<std::string> temp2;
+	// int running_count = temp.size();
+	// temp.resize(200000);
+	// temp2.resize(200000);
+
+	// for (int i = 0; i < 25; i++) {
+	// 	iterate(temp, temp2, running_count);
+	// 	temp.swap(temp2);
+	// 	std::cout << running_count << std::endl;
+	// }
+
+	// res = recursive();
 	return res;
 }
 } // namespace end
